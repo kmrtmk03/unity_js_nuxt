76df8d2306bd16752cc6f6228775c73f6b97be02
kimura <t.kimura@zizo.ne.jp>
2021-11-26 15:10:16 +0900

    modify 文言変更

--- components/Detail.vue
+++ components/Detail.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .detail
+  .detail( :class="{isActive: isShow}" )
     transition(name="fade")
       .inner(v-if="isShow")
         .content
@@ -29,7 +29,7 @@
           {
             title: "Photo-0",
             camera: "SONY RX100",
-            place: "大阪",
+            place: "大阪 あびこ駅ホーム",
             desc: "粒度荒目のモノクロームです"
           },
           {
@@ -47,48 +47,48 @@
           {
             title: "Photo-3",
             camera: "SONY RX100",
-            place: "大阪",
+            place: "大阪 布施駅周辺",
             desc: "粒度荒目のモノクロームです"
           },
           {
             title: "Photo-4",
-            camera: "SONY RX100",
-            place: "大阪",
+            camera: "FUJIFILM X-T2",
+            place: "大阪 大阪城公園",
             desc: "粒度荒目のモノクロームです"
           },
           {
             title: "Photo-5",
-            camera: "SONY RX100",
-            place: "大阪",
+            camera: "Nikon D40",
+            place: "大阪 自宅周辺",
             desc: "粒度荒目のモノクロームです"
           },
           {
             title: "Photo-6",
-            camera: "SONY RX100",
-            place: "大阪",
+            camera: "Nikon D40",
+            place: "大阪 箕面の滝",
             desc: "粒度荒目のモノクロームです"
           },
           {
             title: "Photo-7",
-            camera: "SONY RX100",
-            place: "大阪",
+            camera: "FUJIFILM X-T2",
+            place: "大阪 箕面の滝",
             desc: "粒度荒目のモノクロームです"
           },
           {
             title: "Photo-8",
-            camera: "SONY RX100",
-            place: "大阪",
+            camera: "Nikon D100",
+            place: "京都 嵐山周辺",
             desc: "粒度荒目のモノクロームです"
           },
           {
             title: "Photo-9",
-            camera: "SONY RX100",
-            place: "大阪",
+            camera: "FUJIFILM X-T2",
+            place: "大阪 万博記念公園",
             desc: "粒度荒目のモノクロームです"
           },
           {
             title: "Photo-10",
-            camera: "SONY RX100",
+            camera: "FUJIFILM X-T2",
             place: "大阪",
             desc: "粒度荒目のモノクロームです"
           }
@@ -130,15 +130,24 @@
   }
 
   .detail {
-    // display: none;
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    transition: 0.8s;
+    z-index: 999;
+
+    &.isActive {
+      background-color: rgba(0,0,0,0.5);
+    }
   }
 
   .inner {
     width: 540px;
     height: 100vh;
     background-color: #fff;
-    z-index: 999;
-    position: fixed;
+    position: absolute;
     top: 0;
     left: 0;
 
